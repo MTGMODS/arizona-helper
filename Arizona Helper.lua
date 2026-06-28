2390844060975d870d2635339657d469324d4a12
MTGMODS <bogdan.mtg@gmail.com>
2026-06-28 22:20:18 +0300

    Обновление версии скрипта до 1.8.6 Free

--- Arizona Helper.lua
+++ Arizona Helper.lua
@@ -3,7 +3,7 @@
 script_name("Arizona&Rodina Helper")
 script_description('”ниверсальный хелпер дл€ игроков Arizona Online и Rodina Online')
 script_author("MTG MODS")
-script_version("1.8.6")
+script_version("1.8.6 Free")
 ----------------------------------------------- INIT ---------------------------------------------
 local worked_dir = getWorkingDirectory():gsub('\\','/')
 local IS_MOBILE = MONET_VERSION ~= nil
